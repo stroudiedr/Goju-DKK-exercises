e81db53d6a5ca5c19441446ebabfdcb21c36eaf7
stroudiedr <djfreeman@hotmail.com>
2021-01-23 10:32:01 +0000

    12 not 10 reps

--- sl-app.py
+++ sl-app.py
@@ -16,14 +16,14 @@ st.image(logo,width=350)
 link = '[Goju Karate DKK webpage](http://www.goju-karate.co.uk/)'
 st.markdown(link, unsafe_allow_html=True)
 
-exercises  = {"A":"10 Push Ups","B":"Gekisai Ichi Kata","C":"10 Sit Ups","D":"120 second plank","E":"Gekisai Ni Kata",
-              "F":"Standing sprints (2x60s)","G":"Sepai Kata","H":"10 Squats","I":"10 Push-Ups","J":"Seisan Kata",
-              "K":"Saifa Kata","L":"10 Lunge Skips","M":"10 Crocodile Hops","N":"10 Swallow Pushups",
-              "O":"10 Burpees","P":"10 Squat Thrusts","Q":"Kururnufa Kata","R":"10 Crunches","S":"Seiunchin Kata",
-              "T":"10 Push-Ups","U":"Shisochin Kata","V":"Sanseru Kata","W":"Sanchin Kata","X":"10 Tiger Crawls",
-              "Y":"10 Jumping Squats","Z":"10 Burpees","0":"10 Push-Ups","1":"10 Lunge Skips","2":"10 Squats",
-              "3":"Feet 4' of (60secs on back)","4":"10 Crunches","5":"10 ankle touches (on back)","6":"10 Reverse Crunches",
-              "7":"10 leg raises (on back)","8":"10 crunches","9":"10 burpees"," ":" "
+exercises  = {"A":"12 Push Ups","B":"Gekisai Ichi Kata","C":"12 Sit Ups","D":"120 second plank","E":"Gekisai Ni Kata",
+              "F":"Standing sprints (2x60s)","G":"Sepai Kata","H":"12 Squats","I":"12 Push-Ups","J":"Seisan Kata",
+              "K":"Saifa Kata","L":"12 Lunge Skips","M":"12 Crocodile Hops","N":"12 Swallow Pushups",
+              "O":"12 Burpees","P":"12 Squat Thrusts","Q":"Kururnufa Kata","R":"10 Crunches","S":"Seiunchin Kata",
+              "T":"12 Push-Ups","U":"Shisochin Kata","V":"Sanseru Kata","W":"Sanchin Kata","X":"12 Tiger Crawls",
+              "Y":"12 Jumping Squats","Z":"10 Burpees","0":"12 Push-Ups","1":"12 Lunge Skips","2":"12 Squats",
+              "3":"Feet 4' of (60secs on back)","4":"12 Crunches","5":"12 ankle touches (on back)","6":"12 Reverse Crunches",
+              "7":"12 leg raises (on back)","8":"12 crunches","9":"12 burpees"," ":" "
              }
 
 string = st.text_input("Type in your exercise phrase below")
